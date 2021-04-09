05a4976acaadc9380b99049759af46b9e1bddad1
Dmitry <wodi220500@gmail.com>
2021-04-09 12:51:57 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Dmitriy Chernov on 09.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
