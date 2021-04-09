07f7505d12abb8dee1902ab8e77c34e33746e9bd
Dmitry <wodi220500@gmail.com>
2021-04-09 18:11:29 +0300

    feat: Loading view integration

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -11,21 +11,25 @@ struct AppetizerListView: View {
     @StateObject var viewModel = AppetizerListViewModel()
     
     var body: some View {
-        NavigationView {
-            List(viewModel.appetizers) { appetizer in
-                AppetizerListCell(appetizer: appetizer)
+        ZStack {
+            NavigationView {
+                List(viewModel.appetizers) { appetizer in
+                    AppetizerListCell(appetizer: appetizer)
+                }
+                .navigationTitle("🍔 Appetizers")
             }
-            .navigationTitle("🍔 Appetizers")
-        }
-        .onAppear{
-            viewModel.getAppetizers()
+            .onAppear{
+                viewModel.getAppetizers()
+            }
+            
+            if viewModel.isLoading {
+                LoadingView()
+            }
         }
         .alert(item: $viewModel.alertItem) { alertItem in
             Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
         }
     }
-    
- 
 }
 
 struct AppetizerListView_Previews: PreviewProvider {
